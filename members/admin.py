ff83c07e95f5e91502f1b17737feaa033f427ffa
Vitor Leal <39217722+vitorleal2244@users.noreply.github.com>
2024-08-13 14:52:57 +0100

    [Django] - Project teste

---
+++ members/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Member
+
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+  prepopulated_fields = {"slug": ("firstname", "lastname")}
+
+admin.site.register(Member, MemberAdmin)
+
+# Register your models here.
